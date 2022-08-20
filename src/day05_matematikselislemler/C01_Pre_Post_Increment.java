d315fd2fc0817b3452797aa32b823f5eec4c4ea5
kardesrecep <recepkardes89@gmail.com>
2022-08-20 23:35:00 +0300

    20/08/2022

--- src/day05_matematikselislemler/C01_Pre_Post_Increment.java
+++ src/day05_matematikselislemler/C01_Pre_Post_Increment.java
@@ -1,20 +1,23 @@
 package day05_matematikselislemler;
 
 public class C01_Pre_Post_Increment {
-    int sayi1=10;
-    int sayi2= sayi1+1; //11
+    int sayi1 = 10;
+    int sayi2 = sayi1 + 1; //11
         /* pre veya post increment veya decrement
         sadece ++ ve -- islemi icin gecerlidir.
         bu ik
          */
 
 
-    int sayi3=sayi2++;
+    int sayi3 = sayi2++;
 
-    int sayi4=++sayi1;
+    int sayi4 = ++sayi1;
 
-
-
+    /*
+    x+=y;
+   y=x-y;
+   x=x-y;
+     */
 
 
 }
--- src/lamda_Functional_programming/Utils.java
+++ src/lamda_Functional_programming/Utils.java
@@ -44,5 +44,25 @@ public class Utils {
         return str.charAt(0);
     }
 
+    public static int rakamlarToplaminiAl(int x){
 
+        int toplam = 0;
+
+        while(x>0){
+            toplam += x%10;
+            x /= 10;
+
+        }
+        return toplam;
+    }
 }
+
+
+
+
+
+
+
+
+
+
